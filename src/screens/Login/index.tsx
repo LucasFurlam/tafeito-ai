1f5ea9fc47564542e7ed0a17bee96a884938f908
LucasFurlam <lucasfurlanfreitas@gmail.com>
2023-10-04 23:06:36 -0300

    feat: update route component

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -7,20 +7,22 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import CardActions from '@mui/material/CardActions';
-import { FormControl, InputLabel, 
-        FilledInput, InputAdornment, IconButton, Typography } from '@mui/material';
+import {
+  FormControl, InputLabel,
+  FilledInput, InputAdornment, IconButton, Typography
+} from '@mui/material';
 import { VisibilityOff, Visibility } from '@mui/icons-material'
 
-import {CustomizedCardHeader} from './styles';
+import { CustomizedCardHeader } from './styles';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
   const [isButtonActive, setIsButtonActive] = useState(true);
-  const [username, setUsername] = useState<string|null>(null)
-  const [password, setPassword] = useState<string|null>(null)
+  const [username, setUsername] = useState<string | null>(null)
+  const [password, setPassword] = useState<string | null>(null)
   const [showPassword, setShowPassword] = useState(false);
-  const [errorMessage, setErrorMessage] = useState<string|null>(null)
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   const navigate = useNavigate();
 
@@ -29,23 +31,23 @@ const Login = () => {
   }
 
   useEffect(() => {
-    if(username !== null && username !== '' &&
-       password !== null && password !== '') {
-        setIsButtonActive(false);
-      } else {
-        setIsButtonActive(true);
-      }
+    if (username !== null && username !== '' &&
+      password !== null && password !== '') {
+      setIsButtonActive(false);
+    } else {
+      setIsButtonActive(true);
+    }
   }, [username, password]);
 
   const postLogin = () => {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         login: username,
         senha: password
       })
-  };
+    };
     setErrorMessage('');
     fetch('http://localhost:3000/usuarios/login', requestOptions)
       .then(async (response) => {
@@ -58,42 +60,42 @@ const Login = () => {
       .then(data => {
         console.log('sucesso', JSON.stringify(data))
         console.log(data)
-        if(data.responseStatus === 422 && data.data?.mensagem) {
+        if (data.responseStatus === 422 && data.data?.mensagem) {
           setErrorMessage(data.data?.mensagem)
-        } else if(data.responseStatus === 400) {
+        } else if (data.responseStatus === 400) {
           setErrorMessage('Requisição inválida!')
-        } else if(data.responseStatus === 200) {
+        } else if (data.responseStatus === 200) {
           navigate('/tarefas');
-        } 
+        }
       })
       .catch(error => setErrorMessage('Erro no servidor, tente novamente em alguns minutos!'));
   }
   return (
-      <Box sx={{
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        display: 'flex',
-        justifyContent: 'center'
-      }}
-       >
-        <Card sx={{ maxWidth: 480 }}>
-          <CustomizedCardHeader
-            title="Tafeito"
-            subheader="Transforme suas tarefas em ações"
-          />
-          <CardContent>
-            <Box py={1}>
-              <TextField
-                onChange={(newValue) => {
-                  setUsername(newValue.target.value);
-                }}
-                fullWidth id="username"
-                label="Usuário"
-                variant="filled"
-              />
-            </Box>
-            <Box py={1}>
+    <Box sx={{
+      width: '100%',
+      height: '100%',
+      alignItems: 'center',
+      display: 'flex',
+      justifyContent: 'center'
+    }}
+    >
+      <Card sx={{ maxWidth: 480 }}>
+        <CustomizedCardHeader
+          title="Tafeito"
+          subheader="Transforme suas tarefas em ações"
+        />
+        <CardContent>
+          <Box py={1}>
+            <TextField
+              onChange={(newValue) => {
+                setUsername(newValue.target.value);
+              }}
+              fullWidth id="username"
+              label="Usuário"
+              variant="filled"
+            />
+          </Box>
+          <Box py={1}>
             <FormControl sx={{ width: '100%' }} variant="filled">
               <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
               <FilledInput
@@ -115,34 +117,34 @@ const Login = () => {
                 }
               />
             </FormControl>
-            </Box>
-          </CardContent>
-          <CardActions>
-            <Box sx={{
-              display:'flex',
-              flexDirection:'row',
-              width:'100%',
-              flexWrap: 'wrap'
-            }}>
+          </Box>
+        </CardContent>
+        <CardActions>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '100%',
+            flexWrap: 'wrap'
+          }}>
 
-              <Box width={'100%'}>
-                {errorMessage && <Typography color={'red'}>
-                  {errorMessage}
-                </Typography>}
-              </Box>
-              <Box width={'100%'}>
+            <Box width={'100%'}>
+              {errorMessage && <Typography color={'red'}>
+                {errorMessage}
+              </Typography>}
+            </Box>
+            <Box width={'100%'}>
 
               <Button
                 sx={{
                   width: '100%'
                 }}
-                onClick={() => {postLogin()}}
+                onClick={() => { postLogin() }}
                 disabled={isButtonActive} fullWidth variant="contained">Login</Button>
-              </Box>
             </Box>
-          </CardActions>
-        </Card>
-      </Box>
+          </Box>
+        </CardActions>
+      </Card>
+    </Box>
   )
 }
 
