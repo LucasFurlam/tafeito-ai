5c022da67ac3d39659bdf478aa7d340439f84046
LucasFurlam <lucasfurlanfreitas@gmail.com>
2023-10-04 23:10:14 -0300

    feat: update login and logout routines

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -16,7 +16,11 @@ import { VisibilityOff, Visibility } from '@mui/icons-material'
 import { CustomizedCardHeader } from './styles';
 import { useNavigate } from 'react-router-dom';
 
+import { useAuth } from '../../provider/authProvider';
+
 const Login = () => {
+
+  const { token, setToken } = useAuth();
 
   const [isButtonActive, setIsButtonActive] = useState(true);
   const [username, setUsername] = useState<string | null>(null)
@@ -29,6 +33,12 @@ const Login = () => {
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
   }
+
+  useEffect(() => {
+    if (token) {
+      navigate('/tarefas', { replace: true });
+    }
+  }, [token])
 
   useEffect(() => {
     if (username !== null && username !== '' &&
@@ -65,7 +75,9 @@ const Login = () => {
         } else if (data.responseStatus === 400) {
           setErrorMessage('Requisição inválida!')
         } else if (data.responseStatus === 200) {
-          navigate('/tarefas');
+          if (data?.data?.token) {
+            setToken(data?.data?.token)
+          }
         }
       })
       .catch(error => setErrorMessage('Erro no servidor, tente novamente em alguns minutos!'));
